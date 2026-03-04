991bfe7bea0a46a8167ab460bf335f90dddc06ea
EdiRestrepo <edisonestival@gmail.com>
2026-03-04 11:47:13 -0500

    auth0 java-spring boot

---
+++ construccion_software/activdad1Auth0/ejercicio/src/test/java/com/auth0/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth0.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
