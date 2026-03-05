03660913b64255396fe0bcc953121949cb659045
EdiRestrepo <edisonestival@gmail.com>
2026-03-05 08:46:35 -0500

    update key

--- construccion_software/activdad1Auth0/auth0-angular/src/environments/environment.ts
+++ construccion_software/activdad1Auth0/auth0-angular/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   auth0: {
-    domain: 'dev-5lxfpxxjzz7ikezw.us.auth0.com',
-    clientId: 'qhdT2Y5zfBJSCG90cP2U7S5QFLaQAKDz'
+    domain: '',
+    clientId: ''
   }
 };
